f2233e86c041df8c24aaaeac6e8519a54d06b8a4
yannick <francoyannick04@gmail.com>
2025-04-14 20:06:59 -0300

    implementacion de la solucion limpieza de texto

--- src/palindrome.py
+++ src/palindrome.py
@@ -3,4 +3,15 @@
 def clean_text(text):
     return ''.join(char.lower() for char in text if char.isalnum())
 
+def is_palindrome(text):
+    cleaned = clean_text(text)
+    left = 0
+    right = len(cleaned) - 1
+
+    while left < right:
+        if cleaned[left] != cleaned[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
 
